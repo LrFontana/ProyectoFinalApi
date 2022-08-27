7c05b6e9a8d4a0bad5269cfe0fa3298369970363
Leandro Fontana <leandro_fontana@hotmail.com>
2022-08-27 15:01:14 -0300

    Cambios de eliminacion de codigo y carpetas

--- ProyectoFinalApi/Controllers/DTOS/PostVenta.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProyectoFinalAppi.Controllers.DTOS
-{
-    public class PostVenta
-    {
-        //Variables.
-        public int Id { get; set; }
-        public string Comentarios { get; set; }
-    }
-}
--- ProyectoFinalApi/Controllers/DTOS/PutVenta.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProyectoFinalAppi.Controllers.DTOS
-{
-    public class PutVenta
-    {
-        //Variables.
-        public int Id { get; set; }
-        public string Comentarios { get; set; }
-    }
-}
